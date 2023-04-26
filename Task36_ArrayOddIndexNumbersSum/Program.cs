7a69466c3dc8f62e1fbf220fa946c616040ec8b2
Sergey Zaytsev <surgery_333@mail.ru>
2023-04-26 15:38:19 +0300

    Task36 solved and file with solution added

---
+++ Task36_ArrayOddIndexNumbersSum/Program.cs
@@ -0,0 +1,74 @@
+﻿// Программа по поиску суммы элементов массива , стоящих на нечётных позициях(индексах).
+
+Console.Clear();
+
+int[] arrayConfig = GetArrayConfigFromUser("Введите длину масива, не равную 0: ", "Введите минимальный интервал для значений: ", "Введите максимальный интервал для значений: ", "Ошибка ввода.Повторите попытку!");
+
+int[] array = GetArrayByArrayConfig(arrayConfig);
+Console.WriteLine(String.Join(" ", array));
+
+int result = GetOddIndexNumbersSumByArray(array);
+
+PrintReport(result);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#region Methods
+
+int[] GetArrayConfigFromUser(string message1, string message2, string message3, string errorMessage)
+{
+    int[] arr = new int[3];
+
+    while(true)
+    {
+        Console.Write(message1);
+        if (int.TryParse(Console.ReadLine(), out arr[0]) && arr[0] > 0)
+        {
+            Console.Write(message2);
+            if (int.TryParse(Console.ReadLine(), out arr[1]))
+            {
+                Console.Write(message3);
+                if (int.TryParse(Console.ReadLine(), out arr[2]))
+                {
+                    return arr;
+                }
+            }
+        }
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] GetArrayByArrayConfig(int[] arr)
+{
+    int size = arr[0];
+    int minValue = arr[1];
+    int maxValue = arr[2];
+    int[] res = new int[size];
+
+    for (int i = 0; i < res.Length; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetOddIndexNumbersSumByArray(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + arr[i];
+        }
+    }
+    return sum;
+}
+
+void PrintReport(int number)
+{
+    Console.WriteLine($"Сумма значений, стоящих на нечётных позиция(индексах) в данном массиве, равняется -> {number}");
+}
+
+#endregion
